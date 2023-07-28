b3ea840ee1034628f7459c1aeb6ad44dc3e7d03f
Soham Patil <patilsoham390@gmail.com>
2023-07-29 01:13:23 +0530

    fourth commit

--- Newsproject/settings.py
+++ Newsproject/settings.py
@@ -134,9 +134,9 @@ STATIC_URL = 'static/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
 
 # managing media
 MEDIA_URL = '/media/'
